0e83d0a3acb91cf666d60131541a2d368b20c560
Yago Oliveira <yago.oliveira.ce@protonmail.com>
2020-08-24 01:13:30 -0300

    Implemeting BinaryTree

---
+++ BinaryTree.Client/Nodes/Bst.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryTree.Client
+{
+    public class Bst<T> where T : IComparable<T>
+    {
+        private TreeNode<T> _root;
+
+        public TreeNode<T> Get(T value) => _root?.Get(value);
+
+        public T Min()
+        {
+            if(_root == null)
+                throw new InvalidOperationException("Empty tree");
+            
+            return _root.Min();
+        }
+
+        public T Max()
+        {
+            if(_root == null)
+                throw new InvalidOperationException("Empty tree");
+            
+            return _root.Max();
+        }
+
+        public void Insert(T value)
+        {
+            if(_root == null)
+                _root = new TreeNode<T>(value);
+            else
+                _root.Insert(value);
+        }
+
+        public IEnumerable<T> TraverseInOrder()
+        {
+            if(_root != null)
+                return _root.TraverseInOrder();
+            
+            return Enumerable.Empty<T>();
+        }
+
+        public void Remove(T value)
+        {
+            _root = Remove(_root, value);
+        }
+
+        public TreeNode<T> Remove(TreeNode<T> subTreeRoot, T value)
+        {
+            if(subTreeRoot == null)
+                return null;
+            
+            int compareTo = value.CompareTo(subTreeRoot.Value);
+            if(compareTo < 0)
+            {
+                subTreeRoot.Left = Remove(subTreeRoot.Left, value);
+            }
+            else if(compareTo > 0)
+            {
+                subTreeRoot.Right = Remove(subTreeRoot.Right, value);
+            }
+            else
+            {
+                if(subTreeRoot.Left == null)
+                {
+                    return subTreeRoot.Right;
+                }
+                if(subTreeRoot.Right == null)
+                {
+                    return subTreeRoot.Left;
+                }
+
+                subTreeRoot.Value = subTreeRoot.Right.Min();
+                subTreeRoot.Right = Remove(subTreeRoot.Right, subTreeRoot.Value);
+            }
+
+            return subTreeRoot;
+        }
+    }
+}
---
+++ BinaryTree.Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryTree.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var heap = new MaxHeap<int>();
+            heap.Insert(24);
+            heap.Insert(37);
+            heap.Insert(17);
+            heap.Insert(28);
+            heap.Insert(31);
+            heap.Insert(29);
+            heap.Insert(15);
+            heap.Insert(12);
+            heap.Insert(20);
+
+            // Console.WriteLine(heap.Peek());
+            // Console.WriteLine(heap.Remove());
+            // Console.WriteLine(heap.Peek());
+
+            heap.Insert(40);
+            heap.Sort();
+            // Console.WriteLine();
+
+            foreach(var i in heap.Values())
+            {
+                Console.Write($"{i} ");
+            }
+            Console.Read();
+
+            var tree = new Bst<int>();
+            tree.Insert(13);
+            tree.Insert(2);
+            tree.Insert(5);
+            tree.Insert(16);
+            tree.Insert(15);
+            tree.Insert(20);
+            tree.Insert(10);
+            tree.Insert(7);
+            
+            foreach(var item in tree.TraverseInOrder())
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            tree.Remove(10);
+            foreach(var item in tree.TraverseInOrder())
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.Read();
+        }
+    }
+}
